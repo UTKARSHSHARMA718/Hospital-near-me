077eb73fe9cb71605efdd38b5bedefe8823e6f6f
Utkarsh Sharma <sharmaking894@gmail.com>
2024-04-21 10:02:38 +0530

    feat: removed credentials.js file

--- credentials.js
+++
@@ -1,7 +0,0 @@
-export const REACT_APP_GOOGLE_CLIENT_ID =
-  "738768187644-gahr3g1mo75ck1epa91h32v5hla7j8ch.apps.googleusercontent.com";
-export const REACT_APP_GOOGLE_CLIENT_SECRET =
-  "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";
-export const REACT_APP_GEOAPIFY_API_KEY = "fa1784a72442427089d0e54c74eb1528";
-export const REACT_APP_GOOGLE_MAP_API_KEY =
-  "AIzaSyCvyopwf27yhB3xAelDQfY5ocbFsZPnGf4";
